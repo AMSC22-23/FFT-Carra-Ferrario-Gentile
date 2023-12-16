b95f05955ff9a83280bf4a2379970e33a06f015f
Edoardo Carrà <edocarra012@gmail.com>
2023-12-16 21:49:29 +0000

    add comments to openmp strategy

--- ffft/src/fftcore/Strategy/OpenMP/OmpFFT.hpp
+++ ffft/src/fftcore/Strategy/OpenMP/OmpFFT.hpp
@@ -7,7 +7,6 @@
 #include "../../FFTSolver.hpp"
 #include "../../utils/FFTUtils.hpp"
 
-using namespace std;
 namespace fftcore{
 
     template<typename FloatingType = double>
@@ -39,65 +38,84 @@ namespace fftcore{
     };
 
     /**
+     * Openmp in-place implementation of Cooley-Tuckey algorithm 
      * @author: Edoardo Carra
     */
     template<typename FloatingType>
-    void OmpFFT<FloatingType>::fft(CTensor_1D& input_output, FFTDirection fftDirection) const {
+    void OmpFFT<FloatingType>::fft(CTensor_1D& io_tensor, FFTDirection fft_direction) const {
 
         using Complex = std::complex<FloatingType>;
-        int n = input_output.size();
+        // dimension of the 
+        int n = io_tensor.size();
         Complex w, wm, t, u;
-        int m, m2; 
-		int log2n = std::log2(n);
-		int rev;
+        int m, m2, rev, log2n = std::log2(n);
+
         assert(!(n & (n - 1)) && "FFT length must be a power of 2.");
 
         //conjugate if inverse
-        if(fftDirection == FFT_INVERSE){
-            FFTUtils::conjugate(input_output);
+        if(fft_direction == FFT_INVERSE){
+            FFTUtils::conjugate(io_tensor);
         }
 
-        // Bit-reversal permutation
-		#pragma omp parallel private(m, m2, w, t, u, wm, rev)
-		{
-		#pragma omp for
-        for (int i = 0; i < input_output.size(); ++i)
+
+        #pragma omp parallel private(m, m2, w, t, u, wm, rev)
         {
-            rev = FFTUtils::reverseBits(i, log2n);
-            if (i < rev)
+            /* bit-reversal -> embarassingly parallel, exclusive read and
+            * write access to the tensor. 
+            */
+            #pragma omp for
+            for (int i = 0; i < io_tensor.size(); ++i)
             {
-                std::swap(input_output[i], input_output[rev]);
-            }
-        }
-
-		#pragma omp barrier
-
-        // Cooley-Tukey iterative FFT	
-        for (int s = 1; s <= log2n; ++s) {
-            m = 1 << s;         // 2 power s
-            m2 = m >> 1;        // m2 = m/2 -1
-			wm = exp(Complex(0,-2*M_PI/m));
-
-			#pragma omp for 
-            for (int k = 0; k < n; k += m) {
-				w=Complex(1,0);		    
-//              #pragma omp parallel for private(w,t,u)
-				#pragma omp simd
-				for (int j = 0; j < m2; ++j) {
-                    t = w * input_output[j + k + m2];
-                    u = input_output[j + k];
-                    
-                    input_output[k+j] = u + t;
-                    input_output[k + m2 + j] = u - t;
-					w=w*wm;
+                rev = FFTUtils::reverseBits(i, log2n);
+                if (i < rev)
+                {
+                    std::swap(io_tensor[i], io_tensor[rev]);
                 }
-            }
-        }
-		}
+            }//implicit barrier here, no need to synchronize
+
+            /*
+            * F_1 : outer loop that executes log(n) iterations (stage). Loop-carried data dependence
+            */
+            for (int s = 1; s <= log2n; ++s) {
+                m = 1 << s;         // 2 power s
+                m2 = m >> 1;        // m2 = m/2 -1
+                wm = exp(Complex(0,-2*M_PI/m));
+
+                /*
+                * F_2 : this loop is responsible to the creation of the block partition
+                *       of the input vector. All threads are used until stage s is less then 
+                *       log(n)-log(omp_get_thread_num()). After this threshold is reached, the
+                *       number of threads used is halved at each stage. Loop independent data
+                *       dependence.
+                */
+                #pragma omp for
+                for (int k = 0; k < n; k += m) {
+                    w=Complex(1,0);		    
+                    
+                    /*
+                    *  F_3 : This loop performs the actual computation on each block partition with
+                            a butterfly computation. Loop independent data dependence
+                    */
+                    #pragma omp simd
+                    for (int j = 0; j < m2; ++j) {
+
+                        t = w * io_tensor[j + k + m2];
+                        u = io_tensor[j + k];
+                        
+                        // butterfly
+                        io_tensor[k+j] = u + t;
+                        io_tensor[k + m2 + j] = u - t;
+
+                        w=w*wm; // root of unity
+                    } // F_3 simd
+                }// F_2 implicit barrier here at the end of the stage
+            }//F_1
+        } //end parallel region
+
        //re-conjugate and scale if inverse
-        if(fftDirection == FFT_INVERSE){
-            FFTUtils::conjugate(input_output);
-            input_output = input_output * Complex(1.0/n, 0);
+        if(fft_direction == FFT_INVERSE){
+            FFTUtils::conjugate(io_tensor);
+            io_tensor = io_tensor * Complex(1.0/n, 0);
         }         
     };
 
