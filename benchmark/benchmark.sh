a2afd64994a19ce6633fabf1fedf1db7808b32e3
Edoardo Carrà <edocarra012@gmail.com>
2023-12-18 21:14:03 +0000

    add generalized bnchmark

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -4,6 +4,12 @@ TEST=$1
 PROCESSES_NUM=$2
 
 echo "n,$3 forward,$3 inverse,$4 forward,$4 inverse,SU forward,SU inverse,error"
-for n in {14..19}; do
-	mpirun --use-hwthread-cpus -n $PROCESSES_NUM $TEST $n
+for n in {14..23}; do
+	if grep --quiet -i "MPI" <<< "$1" ;then 
+		test $# -eq 4 || { echo "usage ./compile.sh MPI_processes strategy_header_1 strategy_header_2" >&2; exit; } 
+		mpirun --use-hwthread-cpus -n $PROCESSES_NUM $TEST $n
+	else
+		test $# -eq 3 || { echo "usage ./compile.sh strategy_header_1 strategy_header_2" >&2 ; exit; } 
+		$TEST $n
+	fi
 done
