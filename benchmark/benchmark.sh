023dcf54937df8cc89998e8f02097f258c01efb1
Ferraah <daniele6.ferrario@mail.polimi.it>
2023-12-19 17:18:59 +0000

    Minor fix benchamrk.sh.

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -3,8 +3,13 @@
 TEST=$1
 PROCESSES_NUM=$2
 
-echo "n,$3 forward,$3 inverse,$4 forward,$4 inverse,SU forward,SU inverse,error"
+if grep --quiet -i "MPI" <<< "$1" ;then 
+	echo "n,$3 forward,$3 inverse,$4 forward,$4 inverse,SU forward,SU inverse,error"
+else
+	echo "n,$2 forward,$2 inverse,$3 forward,$3 inverse,SU forward,SU inverse,error"
+fi
 for n in {14..23}; do
+	
 	if grep --quiet -i "MPI" <<< "$1" ;then 
 		test $# -eq 4 || { echo "usage ./compile.sh MPI_processes strategy_header_1 strategy_header_2" >&2; exit; } 
 		mpirun --use-hwthread-cpus -n $PROCESSES_NUM $TEST $n
@@ -13,3 +18,5 @@ for n in {14..23}; do
 		$TEST $n
 	fi
 done
+
+
