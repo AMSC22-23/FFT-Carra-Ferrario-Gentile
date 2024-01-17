95bf7558b5ae0d7265a6abd269d7fbda83e5cdf1
Ferraah <daniele6.ferrario@mail.polimi.it>
2024-01-17 14:44:00 +0000

    Add Spectrogram base code.

--- benchmark/benchmark3D.sh
+++ benchmark/benchmark3D.sh
@@ -8,9 +8,9 @@ if grep --quiet -i "MPI" <<< "$1" ;then
 else
 	echo "n,$2 m,$2 k,$3 forward,$2 inverse,$3 forward,$3 inverse,SU forward,SU inverse,error"
 fi
-for n in {8..10}; do
-	for m in {8..10}; do
-		for k in {2..3}; do	
+for n in {8..9}; do
+	for m in {8..9}; do
+		for k in {8..9}; do	
 			if grep --quiet -i "MPI" <<< "$1" ;then 
 				test $# -eq 4 || { echo "usage ./benchmark.sh test.out MPI_processes strategy_header_1 strategy_header_2" >&2; exit; } 
 				mpirun --use-hwthread-cpus -n $PROCESSES_NUM $TEST $n $m $k
---
+++ ffft/src/spectrogram/Spectrogram.hpp
@@ -0,0 +1,51 @@
+#ifndef SPECTROGRAM_HPP
+#define SPECTROGRAM_HPP
+
+#include "../../include/ffft.hpp"
+
+
+namespace spectrogram{
+    
+	template<class FFTStrategy>
+    class SpectrogramGenerator
+    {
+
+        // Floating type retrieved by the one selected in the solver 
+        using FloatingType = typename FFTStrategy::FloatTypeAlias;
+
+        using CTensor_1D = fftcore::CTensorBase<FloatingType, 1>;
+        using RTensor_1D = fftcore::CTensorBase<FloatingType, 1>;
+        // Spectrogram output
+        using RTensor_2D = fftcore::RTensorBase<FloatingType, 2>;
+
+        public:
+            SpectrogramGenerator(/* args */){};
+            void spectrogram(CTensor_1D &signal, RTensor_2D &spectrogram,  int window_size);
+            void spectrogram(RTensor_1D &signal, RTensor_2D &spectrogram,  int window_size);
+        private:
+
+    };
+
+    /**
+     * @brief 
+     * 
+     * @author Lorenzo Gentile
+    */
+	template<class FFTStrategy>
+    void SpectrogramGenerator<FFTStrategy>::spectrogram(CTensor_1D &signal, RTensor_2D &spectrogram,  int window_size){
+
+        FFTSolver<1, FloatingType> solver = std::make_unique<FFTStrategy>();
+        
+    }
+
+    /**
+     * @todo: I don't think this is needed
+    */
+    template<class FFTStrategy>
+    void SpectrogramGenerator<FFTStrategy>::spectrogram(RTensor_1D &signal, RTensor_2D &spectrogram,  int window_size){
+    }
+}
+
+
+
+#endif
