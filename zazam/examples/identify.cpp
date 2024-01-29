be51fcbc349d50d3243aecd7a9fa5ffec5010989
Ferraah <daniele6.ferrario@mail.polimi.it>
2024-01-29 12:43:21 +0000

    Add examples to zazam.

--- zazam/examples/identify.cpp
+++ zazam/examples/identify.cpp
@@ -11,13 +11,13 @@ using namespace std;
 using namespace zazamcore;
 
 int main(){
-    Identificator identificator("../local_dataset/hashes");
-    Sequencer sequencer("../local_dataset/hashes");
+    Identificator identificator("../zazam/examples/dataset/hashes");
+    Sequencer sequencer("../zazam/examples/dataset/hashes");
     Song sample,result;
-    std::string sample_path = "../local_dataset/samples/redrum_raw.wav";
+    std::string sample_path = "../zazam/examples/dataset/samples/Paranoid_5s_noise.wav";
 
 
-    sequencer.sequence(sample_path, sample, true);
+    sequencer.sequence(sample_path, sample, false);
 
     identificator.identify(sample.hash, result);
 
--- zazam/examples/sequence.cpp
+++ zazam/examples/sequence.cpp
@@ -9,13 +9,14 @@ using namespace std;
 using namespace zazamcore;
 int main(){
 
-    Sequencer sequencer("../local_dataset/hashes");
+    Sequencer sequencer("../zazam/examples/dataset/hashes");
     Song song ;
 
-    std::string wav_path = "../local_dataset/wav";
+    std::string wav_path = "../zazam/examples/dataset/music";
     for (const auto & entry : std::filesystem::directory_iterator(wav_path)){
         std::cout << entry.path() << std::endl;
         std::string path = entry.path().string();
         sequencer.sequence(path, song);
+
     }
 }
